06b9f4b5f6ed6e4cdd3402d2b8e877e06bff0f5b
Edward Moyse <edward.moyse@cern.ch>
2021-07-09 12:15:27 +0200

    Added a new function module It has this incredible function called... plus. This adds things!
    Updating Client to use the new plus function
    This should really improving my adding!
    
    Client2 is updated to use the new plus function.
    Huge improvement in readability.

---
+++ Client.py
@@ -0,0 +1,10 @@
+# Run with:
+# python
+# >>> import Client
+# >>> Client.doSomething()
+from Function import plus
+
+def doSomething():
+    a = 1
+    b = 2
+    print (plus(a,b))
---
+++ Client2.py
@@ -0,0 +1,10 @@
+# Run with:
+# python
+# >>> import Client2
+# >>> Client2.doSomething()
+from Function import plus
+
+def doSomething():
+    a = 1
+    b = 2
+    print (plus(a,b))
